f8ca43e1e90b53d073a73e0cf0e4678976b0d057
Mercy <mercy.chepngetich@student.moringaschool.com>
2023-08-06 00:29:59 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+
+
 // Add your code here
+function submitData(name, email) {
+    const object = { name, email };
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(object),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      document.body.innerHTML = data.id;
+    })
+    .catch((error) => {
+        document.body.innerHTML = error.message;
+      });
+
+}
